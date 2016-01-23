7f684d37a34b6585f38106189011888d11e154c2
Symon799 <simon.rigard@epita.fr>
2016-01-23 17:52:41 +0000

    Added new Air animation Updated ground Added attack animation using F

--- Assets/Scripts/anim.cs
+++ Assets/Scripts/anim.cs
@@ -14,14 +14,33 @@ public class anim : MonoBehaviour {
     }
     void Update()
     {
+        if (Input.GetButtonDown("Attack"))
+            GetComponent<Animator>().SetInteger("state", 1);
+        else if (Input.GetButtonUp("Attack"))
+        {
+            GetComponent<Animator>().SetInteger("state", 2);
+            StartCoroutine("attack");
+        }
+
         if (velo.GetComponent<Rigidbody>().velocity.magnitude > 0.7)
         {
             GetComponent<Animator>().SetBool("speed", true);
+            if (velo.GetComponent<Rigidbody>().velocity.magnitude > 10)
+                GetComponent<Animator>().SetBool("air", true);
+            else
+                GetComponent<Animator>().SetBool("air", false);
+                
         }
         else
         {
             GetComponent<Animator>().SetBool("speed", false);
         }
+    }
+
+    IEnumerator attack()
+    {
+        yield return new WaitForSeconds(0.15f);
+        GetComponent<Animator>().SetInteger("state", 0);
     }
 
     void OnTriggerEnter()
